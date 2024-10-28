2622eef47ceef88b0f69d32254a60fb3cfae925c
mhadej <hubertm@interia.eu>
2024-10-28 19:07:15 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,9 +2,22 @@
 {
     public class Animals
     {
-        public required string Description { get; init; }
+        private string description = "N/A";
+        public required string Description 
+        {
+            get => description;
+            init
+            {
+                description = string.IsNullOrEmpty(value) ? "Unknown" : value.Trim();
+                description = description.Length >= 3 ? description : description + new string('#', (3 - description.Length));
+                description = description.Length > 15 ? description[..15] : description;
+                description = description.Trim();
+                description = description.Length > 15 ? description[..15] : description;
+                description = description.ToUpper()[0] + description[1..description.Length];
+            }
+        }
         public uint Size { get; set; } = 3;
 
-        public void Info() => Console.WriteLine($"{Description} <{Size}>");
+        public string Info => $"{Description} <{Size}>";
     }
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,21 +2,45 @@
 {
     internal class Creature
     {
-        private string? name;
-        private int level;
-        public string? Name { get; set; }
-        public int Level    { get; set; } = 1;
+        private string name = "Unknown";
+        private int level = 1;
+        public string Name
+        {
+            get => name;
+            init
+            {
+                name = string.IsNullOrEmpty(value) ? "Unknown" : value.Trim();
+                name = name.Length >= 3 ? name : name + new string('#', (3 - name.Length));
+                name = name.Length > 25 ? name[..25] : name;
+                name = name.Trim();
+                name = name.Length >= 3 ? name : name + new string('#', (3 - name.Length));
+                name = name.ToUpper()[0] + name[1..name.Length];
+            }
+        }
+        public int Level    
+        { 
+            get => level; 
+            init
+            {
+                level = (value >= 1 && value <= 10) ? value : (value < 1 ? 1 : 10);
+            }
+        }
 
-        public Creature(string name, int level)
+        public Creature(string name, int level = 1)
         {
             Level = level;
-            this.Name = name;
+            Name = name;
         }
 
         public Creature() { }
 
         public void SayHi() => Console.WriteLine($"Hi, my name is {Name}, my level is {Level}");
 
-        public string Info() => $"{Name} - {Level}";
+        public string Info => $"{Name} - {Level}";
+
+        public void Upgrade()
+        {
+            this.level = this.level < 10 ? ++this.level : 10;
+        }
     }
 }
