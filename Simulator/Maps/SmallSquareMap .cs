4b822e9ebaad1d44a707ce890f702c7ba11baa87
mhadej <hubertm@interia.eu>
2024-11-09 19:14:57 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap .cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Maps
+{
+    internal class SmallSquareMap : Map
+    {
+        public readonly int Size;
+
+        public SmallSquareMap(int size)
+        {
+            if(size > 20 || size < 5)
+            {
+                throw new ArgumentOutOfRangeException("Wrong size!");
+            }
+            else
+            {
+                Size = size;
+            }
+        }
+        public override bool Exist(Point p)
+        {
+            Rectangle mapa = new(0, 0, Size-1, Size-1);
+            return mapa.Contains(p);
+        }
+
+        public override Point Next(Point p, Direction d)
+        {
+            Rectangle mapa = new(0, 0, Size - 1, Size - 1);
+            if (mapa.Contains(p.Next(d)) == true)
+            {
+                return p.Next(d);
+            }
+            return p;
+        }
+
+        public override Point NextDiagonal(Point p, Direction d)
+        {
+            Rectangle mapa = new(0, 0, Size - 1, Size - 1);
+            if (mapa.Contains(p.NextDiagonal(d)) == true)
+            {
+                return p.NextDiagonal(d);
+            }
+            return p;
+        }
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Simulator;
+﻿using Simulator.Maps;
+
+namespace Simulator;
 
 internal class Program
 {
@@ -6,8 +8,6 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-
-        Lab5a();
     }
 
     public static void Lab5a()
@@ -18,22 +18,22 @@ internal class Program
             new(5, -1), new(-2, 2)];
 
         Rectangle rec1 = new(4, 2, -2, -2);
-        
-        for(int i = 0; i < pts.Length; i++)
+
+        for (int i = 0; i < pts.Length; i++)
         {
             Console.WriteLine(rec1.Contains(pts[i]));
         }
 
         Console.WriteLine();
 
-        for(int i = 0; i < pts.Length-1; i++)
+        for (int i = 0; i < pts.Length - 1; i++)
         {
             try
             {
-                Rectangle rec2 = new(pts[i], pts[i+1]);
+                Rectangle rec2 = new(pts[i], pts[i + 1]);
                 Console.WriteLine(rec2.ToString());
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -55,6 +55,91 @@ internal class Program
         exception
         no exception
         */
+    }
+
+    public static void Lab5b()
+    {
+        try
+        {
+            SmallSquareMap map = new(10);
+
+            Point point1 = new Point(0, 0);
+            Point point2 = new Point(9, 9);
+            Point point3 = new Point(10, 10);
+            Point point4 = new Point(-1, -1);
+
+            Point randomPoint = new Point(-20, 30);
+
+            Console.WriteLine(map.Exist(point1));
+            Console.WriteLine(map.Exist(point2));
+            Console.WriteLine(map.Exist(point3));
+            Console.WriteLine(map.Exist(point4));
+
+
+            Console.WriteLine(map.Exist(randomPoint));
+
+            /* results:
+            true
+            true
+            false
+            false
+
+            False
+            */
+
+            Console.WriteLine(map.Next(point1, Direction.Up));
+            Console.WriteLine(map.Next(point2, Direction.Right));
+            Console.WriteLine(map.NextDiagonal(point1, Direction.Right));
+
+            /* results:
+            (0, 1)
+            (9, 9)
+            (0, 0)
+            */
+
+            try
+            {
+                SmallSquareMap invalidMap = new SmallSquareMap(25);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Expected Exception: {e.Message}");
+            }
+
+            try
+            {
+                SmallSquareMap invalidMap = new SmallSquareMap(3);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Expected Exception: {e.Message}");
+            }
+
+            Point edgePoint = new Point(9, 9);
+
+            Console.WriteLine(map.Next(edgePoint, Direction.Left));
+            Console.WriteLine(map.NextDiagonal(edgePoint, Direction.Down));
+            Console.WriteLine(map.Next(edgePoint, Direction.Up));
+            Console.WriteLine(map.NextDiagonal(edgePoint, Direction.Up));
+
+            Console.WriteLine(map.NextDiagonal(randomPoint, Direction.Up));
+            Console.WriteLine(map.NextDiagonal(randomPoint, Direction.Down));
+
+            /* results:
+            true
+            (8, 9)
+            (8, 8)
+            (9, 9)
+            (9, 9)
+
+            (-20, 30)
+            (-20, 30)
+            */
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unexpected Exception: {ex.Message}");
+        }
     }
 }
 
